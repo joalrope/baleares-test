11712e1ac04bf7316eedf22ba49ddd4fe479a281
joalrope <joalrope@gmail.com>
2022-05-31 08:58:24 -0400

    rename front-end folder to client

--- api/src/controllers/upload.ts
+++ api/src/controllers/upload.ts
@@ -1,86 +1,65 @@
-import { Request as Req, Response as Resp } from "express";
-import glob from "glob";
-import path from "path";
-import { v4 as uuidv4 } from "uuid";
-import { User } from "../models/User";
+import { Request as Req, Response as Resp } from 'express';
+import glob from 'glob';
+import path from 'path';
+import { v4 as uuidv4 } from 'uuid';
+import { User } from '../models/User';
 
 interface imgFile {
-	id: string;
-	name: string;
-	userId: string;
-	image: string;
+  id: string;
+  name: string;
+  userId: string;
+  image: string;
 }
 
 export const uploadImage = async (req: Req, res: Resp) => {
-	res.status(200).json({
-		ok: true,
-		msg: "Successfully uploaded files",
-	});
+  res.status(200).json({
+    ok: true,
+    msg: 'Successfully uploaded files',
+  });
 };
 
 export const getImage = async (req: Req, res: Resp) => {
-	const { id, userId } = req.params;
-	const url = path.join(__dirname, `../../uploads/images/${userId}/${id}`);
-	res.sendFile(url);
+  const { id, userId } = req.params;
+  const url = path.join(__dirname, `../../uploads/images/${userId}/${id}`);
+  res.sendFile(url);
 };
 
 export const getImages = async (req: Req, res: Resp) => {
-	let result: imgFile[] | undefined = [];
-	const data: string[][] = await searchImages("uploads/images/**/*");
+  let result: imgFile[] | undefined = [];
+  const data: string[][] = await searchImages('uploads/images/**/*');
 
-	data.map(async (item) => {
-		if (item.length > 0) {
-			const [userId, image] = item;
-			let user = await User.findById(userId);
-			console.log(user);
-			const itemJson: imgFile = { id: uuidv4(), name: "cheo", userId, image };
-			result!.push(itemJson);
-		}
-	});
+  data.map(async (item) => {
+    if (item.length > 0) {
+      const [userId, image] = item;
+      let user = await User.findById(userId);
+      console.log(user);
+      const itemJson: imgFile = { id: uuidv4(), name: 'cheo', userId, image };
+      result!.push(itemJson);
+    }
+  });
 
-	res.status(200).json({
-		ok: true,
-		msg: "Successfully catched files",
-		result,
-	});
+  res.status(200).json({
+    ok: true,
+    msg: 'Successfully catched files',
+    result,
+  });
 };
 
 const searchImages = (pattern: string): Promise<string[][]> => {
-	return new Promise((resolve, reject) => {
-		glob(pattern, (error, files) => {
-			if (error) {
-				reject(error);
-				return;
-			}
-			resolve(
-				files.map((file) => {
-					return file
-						.substring(pattern.length - 4)
-						.split("/")
-						.filter((v, i, a) => a.length > 1);
-				})
-			);
-		});
-	});
+  return new Promise((resolve, reject) => {
+    glob(pattern, (error, files) => {
+      if (error) {
+        reject(error);
+        return;
+      }
+      resolve(
+        files.map((file) => {
+          return file
+            .substring(pattern.length - 4)
+            .split('/')
+            .filter((v, i, a) => a.length > 1);
+        })
+      );
+    });
+  });
 };
-
-/* const searchImages = (pattern: string) => {
-	let result: any = [];
-
-	glob(pattern, (err, files) => {
-		if (err) {
-			return err;
-		}
-
-		const jsonFiles = files.map((file) => {
-			const path = file.substring(pattern.length - 4).split("/");
-			if (path.length > 1) {
-				const [userId, name] = path;
-				result.push({ userId, name });
-			}
-			return result;
-		});
-		console.log({ jsonFiles });
-	});
-	console.log({ result });
-}; */
